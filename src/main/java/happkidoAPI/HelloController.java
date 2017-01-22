c0886a7cad0fdbe278ffab405377b0216ee1f536
Rebekah Apelt <rebekah_apelt@thoughtworks.com>
2017-01-22 18:53:36 +1000

    RA first commit

---
+++ src/main/java/happkidoAPI/HelloController.java
@@ -0,0 +1,22 @@
+package happkidoAPI;
+
+/**
+ * Created by rebekahapelt on 22/1/17.
+ */
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Hello greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Hello(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
